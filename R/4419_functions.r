492846ff5a946066064d1e80a08d3b232d0366f1
davidaarmstrong <dave@quantoid.net>
2018-09-07 12:55:26 -0400

    Initial Commit

---
+++ R/4419_functions.r
@@ -0,0 +1,440 @@
+
+inspect <- function(data, x, ...)UseMethod("inspect")
+inspect.tbl_df <- function(data, x, ...){
+  tmp <- data[[as.character(x)]]
+  var.lab <- attr(tmp, "label")
+  if(is.null(var.lab)){var.lab <- "No Label Found"}
+  val.labs <- attr(tmp, "labels")
+  if(is.null(val.labs)){val.labs <- sort(unique(tmp))}
+  tab <- cbind(freq = table(tmp), prop = round(table(tmp)/sum(table(tmp), na.rm=T), 3))
+  out <- list(variable_label = var.lab, value_labels=t(t(val.labs)), freq_dist = tab)
+  return(out)
+}
+inspect.data.frame <- function(data, x, ...){
+  var.lab <- attr(data, "var.label")[which(names(data) == x)]
+  if(is.null(var.lab)){var.lab <- "No Label Found"}
+  val.labs <- if(!is.null(levels(data[[x]]))){levels(data[[x]])}
+    else {sort(unique(data[[x]]))}
+  tab <- cbind(freq = table(data[[x]]), prop = round(table(data[[x]])/sum(table(data[[x]]), na.rm=T), 3))
+  out <- list(variable_label = var.lab, value_labels=t(t(val.labs)), freq_dist = tab)
+  return(out)
+}
+
+## concordant, discordant, tau.b, tau.c, ord.somers.d, ord.gamma come from the ryouready package
+## Phi and V come from the DescTools package
+concordant <- function (x) {
+    x <- matrix(as.numeric(x), dim(x))
+    mat.lr <- function(r, c) {
+        lr <- x[(r.x > r) & (c.x > c)]
+        sum(lr)
+    }
+    r.x <- row(x)
+    c.x <- col(x)
+    sum(x * mapply(mat.lr, r = r.x, c = c.x))
+}
+discordant <- function(x){
+    x <- matrix(as.numeric(x), dim(x))
+    mat.ll <- function(r, c) {
+        ll <- x[(r.x > r) & (c.x < c)]
+        sum(ll)
+    }
+    r.x <- row(x)
+    c.x <- col(x)
+    sum(x * mapply(mat.ll, r = r.x, c = c.x))
+}
+
+tau.b <- function (x) {
+    x <- matrix(as.numeric(x), dim(x))
+    c <- concordant(x)
+    d <- discordant(x)
+    n <- sum(x)
+    SumR <- rowSums(x)
+    SumC <- colSums(x)
+    tau.b <- (2 * (c - d))/sqrt(((n^2) - (sum(SumR^2))) * ((n^2) -
+        (sum(SumC^2))))
+    tau.b
+}
+
+ord.gamma <- function(x){
+    x <- matrix(as.numeric(x), dim(x))
+    c <- concordant(x)
+    d <- discordant(x)
+    gamma <- (c - d)/(c + d)
+    class(gamma) <- "ord.gamma"
+    gamma
+}
+
+ord.somers.d <- function(x){
+    x <- matrix(as.numeric(x), dim(x))
+    c <- concordant(x)
+    d <- discordant(x)
+    n <- sum(x)
+    SumR <- rowSums(x)
+    SumC <- colSums(x)
+    sd.cr <- (2 * (c - d))/((n^2) - (sum(SumR^2)))
+    sd.rc <- (2 * (c - d))/((n^2) - (sum(SumC^2)))
+    sd.s <- (2 * (c - d))/((n^2) - (((sum(SumR^2)) + (sum(SumC^2)))/2))
+    res <- list(sd.cr, sd.rc, sd.s)
+    names(res) <- c("sd.cr", "sd.rc", "sd.symmetric")
+    class(res) <- "ord.somersd"
+    res
+}
+
+lambda <- function(x){
+  wmax <- apply(x, 2, which.max)
+  wgmax <- which.max(rowSums(x))
+  nullcc <- rowSums(x)[wgmax]
+  nullerr <- sum(rowSums(x)[-wgmax])
+  corrpred <- x[cbind(wmax, 1:ncol(x))]
+  errpred <- colSums(x) - corrpred
+  E1 <- nullerr
+  E2 <- sum(errpred)
+  (E1-E2)/E1
+}
+
+phi <- function(x){
+    num <- prod(diag(x))- (x[2,1]*x[1,2])
+    denom <- sqrt(prod(c(colSums(x), rowSums(x))))
+    num/denom
+}
+
+V <- function(x){
+  if(all(dim(x) == 2)){
+    num <- prod(diag(x))- (x[2,1]*x[1,2])
+    denom <- sqrt(prod(c(colSums(x), rowSums(x))))
+    num/denom
+  }
+  else{
+  chi2 <- chisq.test(x, correct=FALSE)$statistic
+  sqrt(chi2/(sum(c(x)) * (min(nrow(x), ncol(x)) -1)))
+  }
+}
+
+simtable <- function(x,y, n=1000, stat=NULL){
+  out <- lapply(1:n, function(i)table(x, sample(y, length(y), replace=F)))
+  if(is.null(stat)){
+    return(out)
+  }
+  else{
+    sapply(out, stat)
+  }
+
+}
+
+simrho <- function(x,y, n=1000){
+  rho0 <- cor(x,y, use="pair", method="spearman")
+  simrho <- sapply(1:n, function(i)cor(x, sample(y, length(y), replace=F), use="pair", method="spearman"))
+  pv <- {if(rho0 >= 0)mean(simrho > rho0)
+      else mean(simrho < rho0)}
+  return(list(rho0 = rho0, simrho = simrho, pv = pv))
+}
+
+makeStats <- function(x,y, chisq=FALSE, phi=FALSE, cramersV=FALSE, lambda=FALSE,
+   gamma=FALSE, d=FALSE, taub=FALSE, rho=FALSE, n=1000){
+
+  tabs <- simtable(x,y,n)
+  tab <- table(x,y)
+allStats <- NULL
+if(chisq){
+  stat0 <- do.call('chisq.test', list(x=tab, correct=FALSE))$statistic
+  stats <- sapply(tabs, function(x)chisq.test(x, correct=FALSE)$statistic)
+  pv <- mean(stats > stat0)
+  allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Chi-squared"
+}
+if(phi){
+  stat0 <- do.call('phi', list(x=tab))
+  stats <- sapply(tabs, function(x)phi(x))
+  pv <- mean(stats > stat0)
+  allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Phi"
+}
+if(cramersV){
+  stat0 <- do.call('V', list(x=tab))
+  stats <- sapply(tabs, function(x)V(x))
+  pv <- mean(stats > stat0)
+  allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Cramers V"
+}
+if(lambda){
+  stat0 <- do.call('lambda', list(x=tab))
+  stats <- sapply(tabs, function(x)lambda(x))
+  pv <- mean(stats > stat0)
+  allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Lambda"
+}
+if(gamma){
+  stat0 <- do.call('ord.gamma', list(x=tab))
+  stats <- sapply(tabs, function(x)lambda(x))
+  pv <- {if(stat0 >= 0)mean(stats > stat0)
+      else mean(stats < stat0)}
+    allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Kruskal-Goodman Gamma"
+}
+if(d){
+  stat0 <- do.call('ord.somers.d', list(x=tab))$sd.symmetric
+  stats <- sapply(tabs, function(x)ord.somers.d(x)$sd.symmetric)
+  pv <- {if(stat0 >= 0)mean(stats > stat0)
+      else mean(stats < stat0)}
+  allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Somers D"
+}
+if(taub){
+  stat0 <- do.call('tau.b', list(x=tab))
+  stats <- sapply(tabs, function(x)tau.b(x))
+  pv <- {if(stat0 >= 0)mean(stats > stat0)
+      else mean(stats < stat0)}
+  allStats <- rbind(allStats, c(stat0, pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Tau-b"
+}
+if(rho){
+  x2 <-as.numeric(x)
+  y2 <- as.numeric(y)
+  r <- simrho(x2,y2,n)
+  allStats <- rbind(allStats, c(r$rho0, r$pv))[,,drop=F]
+  rownames(allStats)[nrow(allStats)] <- "Spearmans Rho"
+}
+if(!is.null(allStats)){
+  colnames(allStats) <- c("statistic", "p-value")
+  w <- which(allStats[,1] == 0 & allStats[,2] == 0)
+  if(length(w) > 0){
+    allStats[w,2] <- 1.000
+  }
+  allStats <- round(allStats, 4)
+}
+return(allStats)
+}
+
+plotStdRes <- function(x, col=RColorBrewer::brewer.pal(10, "RdBu"), ...){
+  x2 <- chisq.test(x)
+  res <- x2$stdres
+  minres <- ifelse(min(c(res)) > -3.5, -3.5, min(c(res)))
+  maxres <- ifelse(max(c(res)) <3.5, 3.5, max(c(res)))
+  if(maxres > abs(minres)){
+    minres <- -maxres
+  }
+  if(maxres < abs(minres)){
+    maxres <- -minres
+  }
+  lattice::levelplot(res, col.regions=col, cuts=10, at=c(minres, -3, -2, -1, 0, 1, 2, 3, maxres), ...)
+}
+
+pmc <- function(obj, col=RColorBrewer::brewer.pal(6, "RdBu"), ...){
+  X <- model.matrix(obj)[,-1]
+  R <- cor(X)
+  diag(R) <- 0
+  return(lattice::levelplot(R, at=c(-1, -.75, -.5, 0, .5, .75, 1), col.regions=col, ..., xlab="", ylab="", scales=list(x=list(rot=90))))
+}
+
+plotCIgroup <- function(form, data, horiz=FALSE,
+    arrowlen = 0, includeOverall=TRUE, distr=c("normal", "t"), conflevel = .95, las=2, ...){
+    mf <- model.frame(form, data)
+    resp <- mf[,1]
+    fac <- mf[,2]
+    if(includeOverall){
+        lfac <- levels(fac)
+        fac <- factor(c(as.character(fac), rep("All Obs", length(resp))), levels=c(lfac, "All Obs"))
+        resp <- c(c(resp), c(resp))
+    }
+    ag <- do.call(rbind, by(resp, list(fac), function(x)confidenceInterval(x, distr=distr,
+       confidence=conflevel)))
+    ngroup <- nrow(ag)
+    if(!horiz){
+        yl <- range(c(ag[,2:3]))
+        xd <- (ngroup-1)*.25
+        xl <- c(1-xd, ngroup + xd)
+        plot(xl, yl, axes=F, type="n", xlab="", ...)
+        points(1:ngroup, ag[,1], ...)
+        axis(1, at=1:ngroup, labels=rownames(ag), las=las)
+        axis(2)
+        box()
+        arrows(1:ngroup, ag[,2], 1:ngroup, ag[,3], code=3, length=arrowlen)
+        if(includeOverall){
+            abline(v=(max(as.numeric(fac))-.5), lty=2)
+        }
+    }
+    if(horiz){
+        yl <- range(c(ag[,2:3]))
+        xd <- (ngroup-1)*.25
+        xl <- c(1-xd, ngroup + xd)
+        plot(yl, xl, axes=F, type="n", ylab="", ...)
+        points(ag[,1], 1:ngroup, ...)
+        axis(2, at=1:ngroup, labels=rownames(ag), las=las)
+        axis(1)
+        box()
+        arrows(ag[,2], 1:ngroup, ag[,3], 1:ngroup, code=3, length=arrowlen)
+        if(includeOverall){
+            abline(h=(max(as.numeric(fac))-.5), lty=2)
+        }
+    }
+}
+searchVarLabels <- function(dat, str) UseMethod("searchVarLabels")
+searchVarLabels.data.frame <-
+function (dat, str)
+{
+    if ("var.labels" %in% names(attributes(dat))) {
+        vlat <- "var.labels"
+    }
+    if ("variable.labels" %in% names(attributes(dat))) {
+        vlat <- "variable.labels"
+    }
+    ind <- sort(union(grep(str, attr(dat, vlat), ignore.case = T), grep(str, names(dat), ignore.case = T)))
+    vldf <- data.frame(ind = ind, label = attr(dat, vlat)[ind])
+    rownames(vldf) <- names(dat)[ind]
+    vldf
+}
+searchVarLabels.tbl_df <-
+function (dat, str)
+{
+    vlat <- unlist(sapply(1:ncol(dat), function(i)attr(dat[[i]], "label")))
+    ind <- sort(union(grep(str, vlat, ignore.case = T), grep(str, names(dat), ignore.case = T)))
+    vldf <- data.frame(ind = ind, label = vlat[ind])
+    rownames(vldf) <- names(dat)[ind]
+    vldf
+}
+
+freqDist <- function(x){
+  tab <- table(x)
+  ntab <- names(tab)
+  pct <- tab/sum(tab)*100
+  cpct <- cumsum(pct)
+  tab <- c(tab, sum(tab))
+  names(tab) <- c(ntab, "Total")
+  pct <- c(pct, 100)
+  cpct <- c(cpct, 100)
+  maxnum <- max(nchar(tab))
+  fr <- sprintf(paste0("%", maxnum, ".0f"), tab)
+  pc <- sprintf("%6.2f", pct)
+  cp <- sprintf("%6.2f", cpct)
+  cp[length(cp)] <- ""
+  out <- cbind(fr, pc, cp)
+  rownames(out) <- names(tab)
+  colnames(out) <- c("Freq", "  %   ", " Cu % ")
+  noquote(out)
+}
+
+histDiscrete <- function(x, ...){
+    l <- max(x, na.rm=T)
+    b <- seq(.5, l+.5, by=1)
+    hist(x, breaks=b, ...)
+}
+
+unalike <- function(x){
+  o <- outer(x, x, "!=")
+  mean(c(o[lower.tri(o)]), na.rm=T)
+}
+
+sig.cor <- function(x,y, method=c("z", "t", "sim"), n.sim = 1000, two.sided=TRUE, ...){
+meth <- match.arg(method)
+r <- cor(x,y, use="pairwise.complete.obs", ...)
+n <- sum(!is.na(x)*!is.na(y))
+if(meth == "z"){
+  z <- .5*log((1+r)/(1-r))
+  sez <- 1/sqrt(n-3)
+  pv <- (2^two.sided)*pnorm(abs(z), 0, sez, lower.tail=F)
+}
+if(meth == "t"){
+  tstat <- r*sqrt((n-2)/(1-r^2))
+  pv <- (2^two.sided)*pt(abs(tstat), n-2, lower.tail=F)
+}
+if(meth == "sim"){
+  xmat <- sapply(1:n.sim, function(z)sample(x, length(x), replace=F))
+  r0 <- c(cor(y, xmat))
+  pv <- {if(two.sided){
+    mean(r0 < (-abs(r))) + mean(r0 > abs(r))
+  }
+  else{
+    if(r > 0){
+      mean(r > r0)
+    }
+    else{
+      mean(r < r0)
+    }
+  }}
+}
+return(list(r=r, p = pv))
+}
+
+
+pwCorrMat <- function(X, method=c("z", "t", "sim"), ...){
+  meth <- match.arg(method)
+  out <- p.out <- diag(ncol(X))
+  for(i in 1:(ncol(X)-1)){
+    for(j in i:ncol(X)){
+      f <- sig.cor(X[,i], X[,j], method=meth, ...)
+      out[i,j] <- out[j,i] <- f$r
+      p.out[i,j] <- p.out[j,i] <- f$p
+    }
+  }
+  outSig <- matrix(sprintf("%.3f", out), ncol=ncol(X))
+  outSig[which(p.out > .05, arr.ind=T)] <- ""
+  diag(outSig) <- ""
+  outSig[upper.tri(outSig)] <- ""
+  colnames(outSig) <- colnames(out) <- rownames(outSig) <- rownames(out) <- colnames(p.out) <- rownames(p.out) <- colnames(X)
+  ret <- list(rSig=outSig, r=out, p = p.out )
+  class(ret) <- "pwc"
+  return(ret)
+}
+
+print.pwc <- function(x, ...){
+cat("All Correlations\n")
+ print(noquote(round(x$r,3)))
+cat("\nOnly Significant Correlations\n")
+ print(noquote(x$rSig))
+}
+
+
+GKGamma <- function (x, y = NULL, conf.level = NA, ...){
+## Function taken from DescTools v0.99.22
+    if (!is.null(y))
+        tab <- table(x, y, ...)
+    else tab <- as.table(x)
+    x <- ConDisPairs(tab)
+    psi <- 2 * (x$D * x$pi.c - x$C * x$pi.d)/(x$C + x$D)^2
+    sigma2 <- sum(tab * psi^2) - sum(tab * psi)^2
+    gamma <- (x$C - x$D)/(x$C + x$D)
+    if (is.na(conf.level)) {
+        result <- gamma
+    }
+    else {
+        pr2 <- 1 - (1 - conf.level)/2
+        ci <- qnorm(pr2) * sqrt(sigma2) * c(-1, 1) + gamma
+        result <- c(gamma = gamma, lwr.ci = max(ci[1], -1), ups.ci = min(ci[2],
+            1))
+    }
+    class(result) <- "gkg"
+    return(result)
+}
+
+confidenceInterval <- function (x, confidence = 0.95,  na.rm = TRUE, distr=c("normal", "t")){
+    distr <- match.arg(distr)
+    nobs <- sum(!is.na(x))
+    est <- mean(x, na.rm = na.rm)
+    stderr <- sd(x, na.rm = na.rm)/sqrt(nobs)
+    alpha <- 1-confidence
+    if(distr == "t"){
+      ci.low <- est + qt(alpha/2, nobs - 1) * stderr
+      ci.high <- est - qt(alpha/2, nobs - 1) * stderr
+    }
+    else{
+      ci.low <- est + qnorm(alpha/2) * stderr
+      ci.high <- est - qnorm(alpha/2) * stderr
+    }
+    retval <- c(Estimate = est, `CI lower` = ci.low, `CI upper` = ci.high,
+        `Std. Error` = stderr)
+    retval
+}
+
+print.gkg <- function(x, ...){
+  if(class(x) != "gkg")stop("Object must be of class gkg\n")
+  if(length(x) == 1){
+    cat("Goodman-Kruskal's Gamma = ", round(x,3), "\n", sep="")
+  }
+  if(length(x) == 3){
+    cat("Goodman-Kruskal's Gamma = ", round(x,3), ", 95% CI (", round(x[2], 3), ", ", round(x[3],3),  ")\n", sep="")
+  }
+}
+print.ktb <- function(x, ...){
+  if(class(x) != "ktb")stop("Object must be of class ktb\n")
+  cat("Kendall's Tau-b = ", round(x,3), "\n", sep="")
+}
